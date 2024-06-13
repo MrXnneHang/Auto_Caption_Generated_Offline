931f56fb1be5fed6276e4a244b56af9b520b36e0
MrXnneHang <XnneHang@gmail.com>
2024-06-13 15:49:58 +0800

    更改示例

--- run_srt.py
+++ run_srt.py
@@ -9,10 +9,16 @@ from txt_to_srt import convert_to_srt, remove_chinese_commas_and_periods
 
 """
 
-wav_name = "08"
 def main(wav_name):
     print("开始语音识别")
-    write_long_txt(wav_name=wav_name,cut_line=500000) ##./tmp/.txt
+    with open("./hot_words.txt",'r',encoding="utf-8") as f:
+        lines = f.readlines()
+    hot_words = ""
+    for line in lines:
+        hot_words += line[:-1] #不加换行
+        hot_words += ","
+    #print(hot_words)
+    write_long_txt(wav_name=wav_name,cut_line=500000,hot_word=hot_words) ##./tmp/.txt
 
     #print("开始处理识别后的语句")
     #convert_short_txt_to_long(wav_name=wav_name) ## ./tmp/processed.txt
@@ -34,6 +40,11 @@ if __name__ == "__main__":
     file_names = os.listdir("./raw_audio")
     if "desktop.ini" in file_names:
         file_names.remove("desktop.ini")
+    if "put wav files here.txt" in file_names:
+        file_names.remove("put wav files here.txt")
     for i in tqdm(range(len(file_names))):
+        if not file_names[i].endswith(".wav"):
+            print("并不是支持的wav格式,skip..")
+            continue
         main(wav_name=file_names[i].split(".")[0])
     print("All process were done!")
