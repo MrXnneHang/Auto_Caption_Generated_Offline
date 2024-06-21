f492ecbc935068cf36478c3f10e0fb7c4afffa5b
MrXnneHang <XnneHang@gmail.com>
2024-06-21 18:31:30 +0800

    合并短句，不知不觉bug自己没了

--- short_text_to_long.py
+++ short_text_to_long.py
@@ -4,6 +4,7 @@ def convert_short_txt_to_long(wav_name,combine_line):
     latest_start = 0
     latest_end = 0
     latest_content = ""
+    latest_line = ""
     new_lines = []
     with open(f'./tmp/{wav_name}.txt', 'r', encoding='utf-8') as file:
         for line in file:
@@ -11,11 +12,11 @@ def convert_short_txt_to_long(wav_name,combine_line):
             content = line.split("|")[2]
             if (int(start_and_end[0])-int(latest_end)<combine_line) and (int(latest_end)!=0): #合并这两行
                 new_line = latest_start+"|"+start_and_end[1]+"|"+latest_content.replace("\n","")+content.replace("\n","")
+
+                new_lines.pop()
                 new_lines.append(new_line)
             else:
                 new_lines.append(line.replace("\n",""))
-            latest_content = content
-            latest_start=start_and_end[0]
-            latest_end=start_and_end[1]
+
 
     write_lines_to_file(f"./tmp/processed_{wav_name}.txt",new_lines)
