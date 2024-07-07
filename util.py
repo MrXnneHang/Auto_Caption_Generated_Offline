da9b39ddc557385c4d13597d0b9fc2ac2dea15f1
MrXnneHang <XnneHang@gmail.com>
2024-07-07 10:10:28 +0800

    加入生成不同模型的选项

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import yaml
 import os
-
+from funasr import AutoModel
+from datetime import datetime
 
 def load_config():
 
@@ -15,3 +16,74 @@ def load_config():
             config = yaml.safe_load(file)
 
         return config
+class FunASRModel:
+
+    def __init__(self):
+        """
+        """
+        self.config = load_config()
+        self.base_model = self.config["base_model"]
+        self.vad_model = self.config["vad_model"]
+        self.punc_model = self.config["punc_model"]
+        self.device = self.config["device"]
+
+    def full_version(self):
+        funasr_model = AutoModel(
+            model=self.base_model,  # base
+            vad_model=self.vad_model,  # 检测语音活动，自动分隔
+            punc_model=self.punc_model,  # 给出标点。
+            # model_revision="v2.0.4",
+            device=self.device
+        )
+        return funasr_model
+
+    def only_vad(self):
+        model = AutoModel(model=self.vad_model,
+                          device=self.device)
+        return model
+
+    def only_txt(self):
+        model = AutoModel(model = self.base_model,
+                          device=self.device)
+
+
+def generate_results(model,wav_name, hot_word="",debug=False):
+    if type(wav_name) is list:
+        if hot_word!="":
+            res = model.generate(
+                input=wav_name,
+                hotword=hot_word,
+                batch_size_s=300,
+            )
+        else:
+            res = model.generate(
+                input=wav_name,
+                batch_size_s=300,
+            )
+    else:
+        if hot_word!="":
+            res = model.generate(
+                input=f"./raw_audio/{wav_name}.wav",
+                hotword=hot_word,
+                batch_size_s=300,
+            )
+        else:
+            res = model.generate(
+                input=f"./raw_audio/{wav_name}.wav",
+                batch_size_s=300,
+            )
+
+    return res
+
+def write_long_txt(response):
+
+    # 获取当前时间，并创建带时间戳的文件夹
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M")
+    output_dir = os.path.join("./tmp", timestamp)
+    os.makedirs(output_dir, exist_ok=True)
+    print("开始保存文本")
+    # 将 文本文件保存到带时间戳的文件夹中
+    srt_path = os.path.join(output_dir,"only_text.txt")
+    with open(srt_path, 'w', encoding='utf-8') as file:
+        file.write(response[0]["text"])
+    return timestamp
