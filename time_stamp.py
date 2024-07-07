aa0f19a503f3f38c13eda99201f175200eaeefad
MrXnneHang <XnneHang@gmail.com>
2024-07-07 10:10:02 +0800

    no_change

--- time_stamp.py
+++ time_stamp.py
@@ -1,32 +1,15 @@
-from funasr import AutoModel
+from util import  FunASRModel,generate_results
 from sentences_method import generate_new_sentences
 from automodel_rec_to_sentences import convert_format,remove_chinese_punctuation #把automodel 返回的rec转换成以前Pipline的格式。
 from automodel_rec_to_sentences import calculate_length,segment_text,split_into_words  #用来debug，检测两句是否一样长，是否有标点符号没去掉。
 
-# 定义模型
-model = AutoModel(
-    model="./models/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch", # base
-    vad_model="./models/speech_fsmn_vad_zh-cn-16k-common-pytorch", #支持长音频，自动分隔
-    punc_model="./models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch", # 检测语音活动，给出标点。
-    #model_revision="v2.0.4",
-    device="cuda:0"
-)
-
-
-# 生成结果并写入文件
-def generate_results(funasr_model,wav_name, hot_word,debug=False):
-    res = funasr_model.generate(
-        input=f"./raw_audio/{wav_name}.wav",
-        hotword=hot_word,
-        batch_size_s=300,
-    )
-    return res
-
 
 
 # 定义长文本写入函数
-def write_long_txt(wav_name, cut_line,hot_word,debug=True):
-    response = generate_results(funasr_model=model,wav_name=wav_name,hot_word=hot_word)
+def write_long_txt_with_timestamp(wav_name, cut_line,hot_word,debug=True):
+    Model = FunASRModel()
+    model = Model.full_version()
+    response = generate_results(model=model,wav_name=wav_name,hot_word=hot_word)
     if debug == True:
         # print(remove_chinese_punctuation(response[0]["text"]))
         sentences = convert_format(response,debug=True)
@@ -65,6 +48,7 @@ def write_long_txt(wav_name, cut_line,hot_word,debug=True):
                 # else:
                 #     continue
     write_lines_to_file(f'./tmp/{wav_name}.txt', lines)
+    return response
 
 
 # 写入行到文件
@@ -75,4 +59,4 @@ def write_lines_to_file(file_path, lines):
 
 
 if __name__ == "__main__":
-    write_long_txt(wav_name="example",cut_line=1000000,hot_word="",debug=True)
+    write_long_txt_with_timestamp(wav_name="example",cut_line=1000,hot_word="",debug=True)
